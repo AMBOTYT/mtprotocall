9e809a38bd88fd16582c3422c67523bc7dc2f2e3
AMBOTYT <142726836+AMBOTYT@users.noreply.github.com>
2023-10-22 04:14:13 +0530

    Update check_user.py

--- handlers/check_user.py
+++ handlers/check_user.py
@@ -1,10 +1,6 @@
-
-
 import datetime
-
 import config
 import logging
-
 from handlers.database import Database
 
 DB_URL = config.DB_URL
@@ -23,11 +19,13 @@ async def handle_user_status(bot, cmd):
             await bot.send_message(
                 LOG_CHANNEL,
                 f"""#NEW_USERS
-                𝙉𝙚𝙬 𝙐𝙨𝙚𝙧: [{message.from_user.first_name}](tg://user?id={message.from_user.id})
-                𝘽𝙤𝙩 𝙐𝙨𝙚𝙧𝙉𝙖𝙢𝙚 : @{BOT_USERNAME}\n┣★ 𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿 : @AM_YTBOTT""",
+                ┏━━━━━━━━━━━━┓
+                ┣★ 𝙉𝙚𝙬 𝙐𝙨𝙚𝙧: [{cmd.from_user.first_name}](tg://user?id={cmd.from_user.id})
+                ┣★ 𝘽𝙤𝙩 𝙐𝙨𝙚𝙧𝙉𝙖𝙢𝙚 : @{BOT_USERNAME}
+                ┣★ 𝗕𝗼𝘁 𝗢𝘄𝗻𝗲𝗿 : @AM_YTBOTT""",
             )
         else:
-            logging.info(f"#NewUser\nNew User : {message.from_user.first_name} \nUser Id : {message.from_user.id}")
+            logging.info(f"#NewUser\nNew User : {cmd.from_user.first_name} \nUser Id : {cmd.from_user.id}")
 
     ban_status = await db.get_ban_status(chat_id)
     if ban_status["is_banned"]:
@@ -36,6 +34,6 @@ async def handle_user_status(bot, cmd):
         ).days > ban_status["ban_duration"]:
             await db.remove_ban(chat_id)
         else:
-            await cmd.reply_text("You are Banned to Use This Bot ", quote=True)
+            await cmd.reply_text("You are Banned from using this Bot", quote=True)
             return
     await cmd.continue_propagation()
