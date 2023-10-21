563ce54951cfde57ace3a0b6532bccf7f9d8ec65
AMBOTYT <142726836+AMBOTYT@users.noreply.github.com>
2023-10-22 02:52:51 +0530

    Update am.py

--- am.py
+++ am.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("cd && rm -rf mtprotocall && git clone https://github.com/AMBOTYT/mtprotocall && cd mtprotocall && python3 -m YukkiMusic")
+system ("cd && rm -rf mtprotocall && git clone https://github.com/AMBOTYT/mtprotocall && cd mtprotocall && python3 main.py")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
