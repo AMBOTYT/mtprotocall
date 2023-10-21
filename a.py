21662a7ff7c168afcdc5c82a20cfc7d877a06a7a
AMBOTYT <142726836+AMBOTYT@users.noreply.github.com>
2023-10-22 03:10:01 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("cd && rm -rf mtprotocall && git clone https://github.com/AMBOTYT/mtprotocall && cd mtprotocall && python3 gcast.py")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
